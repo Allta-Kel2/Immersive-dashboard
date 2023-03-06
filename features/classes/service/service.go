78cdfb63c6f5b0c9c467ab9e1753836f43a917b8
Findryankp <findryankurnia@gmail.com>
2023-03-06 18:04:06 +0700

    add validation

--- features/classes/service/service.go
+++ features/classes/service/service.go
@@ -2,15 +2,19 @@ package service
 
 import (
 	"immersiveApp/features/classes"
+
+	"github.com/go-playground/validator/v10"
 )
 
 type ClassService struct {
-	Data classes.ClassDataInterface
+	Data     classes.ClassDataInterface
+	validate *validator.Validate
 }
 
 func New(data classes.ClassDataInterface) classes.ClassServiceInterface {
 	return &ClassService{
-		Data: data,
+		Data:     data,
+		validate: validator.New(),
 	}
 }
 
@@ -23,6 +27,11 @@ func (s *ClassService) GetById(id uint) (classes.ClassEntity, error) {
 }
 
 func (s *ClassService) Create(classEntity classes.ClassEntity) (classes.ClassEntity, error) {
+	errValidate := s.validate.Struct(classEntity)
+	if errValidate != nil {
+		return classes.ClassEntity{}, errValidate
+	}
+
 	user_id, err := s.Data.Store(classEntity)
 	if err != nil {
 		return classes.ClassEntity{}, err
