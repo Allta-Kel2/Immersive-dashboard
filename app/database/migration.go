302dae396921ecaf28728b5f4916ed127bd3e16b
Rischiyuda <rischiyuda17@gmail.com>
2023-03-07 08:57:41 +0700

    [feature] status-GET

--- app/database/migration.go
+++ app/database/migration.go
@@ -2,6 +2,8 @@ package database
 
 import (
 	"fmt"
+	class "immersiveApp/features/classes/data"
+	status "immersiveApp/features/statuses/data"
 	team "immersiveApp/features/teams/data"
 	user "immersiveApp/features/users/data"
 
@@ -12,6 +14,8 @@ func InitMigration(db *gorm.DB) {
 	err := db.AutoMigrate(
 		&user.User{},
 		&team.Team{},
+		&class.Class{},
+		&status.Status{},
 	)
 
 	if err != nil {
