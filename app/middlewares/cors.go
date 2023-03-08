c6b8cf6175c7d1dd305c503b5c67fa04c9e543f6
Findryankp <findryankurnia@gmail.com>
2023-03-08 18:06:21 +0700

    cord

--- app/middlewares/cors.go
+++ app/middlewares/cors.go
@@ -1,6 +1,8 @@
 package middlewares
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -9,5 +11,6 @@ func Cors(e *echo.Echo) {
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
 		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+		AllowMethods: []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
 	}))
 }
