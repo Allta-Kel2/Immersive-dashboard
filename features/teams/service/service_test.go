293a3635d9010790b340a69d744da41f8c126fb0
Rischiyuda <rischiyuda17@gmail.com>
2023-03-08 16:00:03 +0700

    [mocks-teams] build unit-test

--- features/teams/service/service_test.go
+++ features/teams/service/service_test.go
@@ -4,35 +4,31 @@ import (
 	tims "immersiveApp/features/teams"
 	"immersiveApp/mocks"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 )
 
 func TestCreate(t *testing.T) {
 	repo := mocks.NewTeamDataInterface(t)
-	expected := tims.TeamEntity{
-		Id:        uint(1),
-		Name:      "mentor",
-		CreatedAt: time.Time{},
-		UpdatedAt: time.Time{},
-	}
 	input := tims.TeamEntity{
-		Id:        uint(1),
-		Name:      "",
-		CreatedAt: time.Time{},
-		UpdatedAt: time.Time{},
+		Id:   uint(1),
+		Name: "",
 	}
 	srv := New(repo)
-	t.Run("Sukses Add", func(t *testing.T) {
-		repo.On("Insert", mock.Anything).Return(expected, nil).Once()
 
-		_, err := srv.Create(expected)
-		assert.Nil(t, err)
-		assert.Empty(t, err)
-		repo.AssertExpectations(t)
-	})
+	mockTeamEntity := tims.TeamEntity{
+		Name: "Team A",
+	}
+	repo.On("Store", mockTeamEntity).Return(uint(1), nil)
+	repo.On("SelectById", uint(1)).Return(mockTeamEntity, nil)
+
+	created, err := srv.Create(mockTeamEntity)
+
+	assert.Nil(t, err)
+	assert.Equal(t, mockTeamEntity, created)
+
+	repo.AssertExpectations(t)
+
 	t.Run("eror", func(t *testing.T) {
 		_, err := srv.Create(input)
 		assert.NotEmpty(t, err)
@@ -40,3 +36,76 @@ func TestCreate(t *testing.T) {
 		repo.AssertExpectations(t)
 	})
 }
+
+func TestGetAll(t *testing.T) {
+	repo := mocks.NewTeamDataInterface(t)
+	mockTeam := tims.TeamEntity{
+		Id:   0,
+		Name: "team1",
+	}
+
+	mockListTeam := make([]tims.TeamEntity, 0)
+	mockListTeam = append(mockListTeam, mockTeam)
+
+	repo.On("SelectAll").Return(mockListTeam, nil)
+
+	srv := New(repo)
+
+	result, err := srv.GetAll()
+
+	repo.AssertExpectations(t)
+	assert.NoError(t, err)
+	assert.Len(t, result, len(mockListTeam))
+	assert.Equal(t, mockListTeam, result)
+
+}
+func TestGetById(t *testing.T) {
+	repo := mocks.NewTeamDataInterface(t)
+	mockTeam := tims.TeamEntity{
+		Id:   1,
+		Name: "team1",
+	}
+
+	repo.On("SelectById", mockTeam.Id).Return(mockTeam, nil)
+
+	srv := New(repo)
+
+	result, err := srv.GetById(mockTeam.Id)
+	repo.AssertExpectations(t)
+	assert.NoError(t, err)
+	assert.Equal(t, mockTeam, result)
+}
+
+func TestUpdate(t *testing.T) {
+	repo := mocks.NewTeamDataInterface(t)
+	mockTeamEntity := tims.TeamEntity{
+		Name: "Team A",
+	}
+
+	repo.On("Update", mockTeamEntity.Id).Return(mockTeamEntity, nil).Once()
+
+	srv := New(repo)
+
+	result, err := srv.Update(tims.TeamEntity{Name: "New Team Name"}, uint(1))
+
+	assert.NoError(t, err)
+	assert.Equal(t, uint(1), result.Id)
+	assert.Equal(t, "Update", result.Name)
+}
+
+// func TestDelete(t *testing.T) {
+// 	repo := mocks.NewTeamDataInterface(t)
+// 	srv := New(repo)
+// 	t.Run("Delete Success", func(t *testing.T) {
+// 		repo.On("Delete", 1, 1).Return(nil).Once()
+
+// 		_, token := helpers.GenerateJWT(1)
+// 		pToken := token.(*jwt.Token)
+// 		pToken.Valid = true
+// 		err := srv.Delete(pToken, 1, 1)
+
+// 		assert.Nil(t, err)
+
+// 		repo.AssertExpectations(t)
+// 	})
+// }
