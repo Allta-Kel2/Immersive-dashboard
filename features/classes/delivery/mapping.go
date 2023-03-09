fe2f38a47925377ef974d9cace45411bdfe94209
Findryankp <findryankurnia@gmail.com>
2023-03-10 02:42:46 +0700

    mapping data

--- features/classes/delivery/mapping.go
+++ features/classes/delivery/mapping.go
@@ -1,6 +1,10 @@
 package delivery
 
-import "immersiveApp/features/classes"
+import (
+	"immersiveApp/features/classes"
+	"immersiveApp/features/users/delivery"
+	"reflect"
+)
 
 func ClassRequestToClassEntity(classRequest *ClassRequest) classes.ClassEntity {
 	return classes.ClassEntity{
@@ -12,13 +16,28 @@ func ClassRequestToClassEntity(classRequest *ClassRequest) classes.ClassEntity {
 }
 
 func ClassEntityToClassResponse(classEntity classes.ClassEntity) ClassResponse {
-	return ClassResponse{
+	result := ClassResponse{
 		Id:           classEntity.Id,
 		ClassName:    classEntity.ClassName,
 		PicUserId:    classEntity.PicUserId,
 		DateStart:    classEntity.DateStart,
 		DateGraduate: classEntity.DateGraduate,
 	}
+
+	if !reflect.ValueOf(classEntity.User).IsZero() {
+		result.User = &delivery.UserResponse{
+			Id:          classEntity.Id,
+			TeamId:      classEntity.User.TeamId,
+			FullName:    classEntity.User.FullName,
+			Email:       classEntity.User.Email,
+			Role:        classEntity.User.Role,
+			Status:      classEntity.User.Status,
+			PhoneNumber: classEntity.User.PhoneNumber,
+			Address:     classEntity.User.Address,
+		}
+	}
+
+	return result
 }
 
 func ListClassEntityToClassResponse(classEntity []classes.ClassEntity) []ClassResponse {
